7a7e0c8b5dab5f6f8041373b107da93dabb5e3c4
Daniel Stonier <d.stonier@gmail.com>
2012-11-17 23:54:56 +0900

    ecl_config catkinized.

--- ecl_config/cmake/Version.cmake
+++
@@ -1,8 +0,0 @@
-##############################################################################
-# ECL Version
-##############################################################################
-
-SET(PROJECT_VERSION "0.40.0")
-SET(PROJECT_VERSION_MAJOR "0")
-SET(PROJECT_VERSION_MINOR "40")
-SET(PROJECT_VERSION_PATCH "0")
