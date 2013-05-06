e33f875a1a2d66ab15a69f88d94cc30c85156d30
Daniel Stonier <d.stonier@gmail.com>
2013-05-06 10:09:30 +0900

    fiasco testing

---
+++ ecl_foo/f.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file /ecl_config/src/config.cpp
+ *
+ * @brief File comment
+ *
+ * File comment
+ *
+ * @date 06/05/2013
+ **/
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <iostream>
+#include "f.hpp"
+
+void fooDude() {
+  std::cout << "fooDude::Hello Dude" << std::endl;
+  std::cout << "What a " << fiasco() << std::endl;
+}
---
+++ ecl_foo/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file /ecl_config/src/main.cpp
+ *
+ * @brief File comment
+ *
+ * File comment
+ *
+ * @date 06/05/2013
+ **/
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <iostream>
+#include "f.hpp"
+
+int main() {
+  std::cout << "Hello Dude" << std::endl;
+  fooDude();
+  fooDude();
+  fooDude();
+  std::cout << "Yeah, it's a " << fiasco() << std::endl;
+  return 0;
+}
