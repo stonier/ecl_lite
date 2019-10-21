6f597e81b4933ef2362a4ac1fa25098ce1234216
seanyen <seanyen@microsoft.com>
2019-10-21 13:45:13 -0700

    MSVC build break fixes.

--- ecl_errors/include/ecl/errors/compile_time_assert.hpp
+++ ecl_errors/include/ecl/errors/compile_time_assert.hpp
@@ -58,7 +58,7 @@ template <int x> struct static_assert_test {};
  * the unit testing facility.
  */
 #define ecl_compile_time_assert( logical_expression ) \
-   typedef ecl::static_assert_test<sizeof(ecl::COMPILE_TIME_FAILURE< static_cast<bool>(logical_expression) >)> JOIN(compile_time_check,__LINE__) __attribute__((unused))
+   typedef ecl::static_assert_test<sizeof(ecl::COMPILE_TIME_FAILURE< static_cast<bool>(logical_expression) >)> JOIN(compile_time_check,__LINE__) [[gnu::unused]]
 
 /**
  * @brief Verbose compile time assert.
